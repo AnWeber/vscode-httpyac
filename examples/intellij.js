1f886189d5d9cea3bf95900a5d4ec84aa8e95d1b
Andreas Weber <weber.andreas@gmail.com>
2021-02-24 15:41:31 +0100

    fix: test cases for intellij updated (AnWeber/vscode-httpyac#14)

--- examples/intellij.js
+++ examples/intellij.js
@@ -1,5 +1,5 @@
-client.global.set("search", "test");
-  client.test("Request executed successfully", function() {
 
-      client.assert(response.status === 200, "Response status is not 200");
-  });
+client.global.set("email", response.body.form.email);
+client.test("Request executed successfully", function() {
+    client.assert(response.status === 200, "Response status is not 200");
+});
