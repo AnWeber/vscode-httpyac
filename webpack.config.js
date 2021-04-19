28f45dcd5bd5322c7caac53bdbde6b9c8d25e120
Andreas Weber <weber.andreas@gmail.com>
2021-04-19 20:59:11 +0200

    build: faster webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,71 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-/**@type {import('webpack').Configuration}*/
-const config = {
-  target: 'node',
-	mode: 'none',
 
-  entry: './src/extension.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    library: {
-      type: 'commonjs2',
+module.exports = (env, argv) => {
+
+  /**@type {import('webpack').Configuration}*/
+  const config = {
+    target: 'node',
+    mode: 'none',
+
+    entry: './src/extension.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'extension.js',
+      library: {
+        type: 'commonjs2',
+      },
     },
-  },
-  devtool: 'nosources-source-map',
-  externals: {
-    vscode: 'commonjs vscode'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ContextReplacementPlugin(/keyv/),
-    new ESLintPlugin({extensions: ['js', 'ts']})
-  ]
-};
-module.exports = config;
+    devtool: argv.mode === 'development' ? 'eval-cheap-module-source-map' : 'nosources-source-map',
+    externals: {
+      vscode: 'commonjs vscode'
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'thread-loader',
+              options: {
+                // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+                workers: require('os').cpus().length - 1,
+              },
+            },
+            {
+              loader: 'ts-loader',
+              options: {
+                happyPackMode: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ContextReplacementPlugin(/keyv/),
+      new ForkTsCheckerWebpackPlugin({
+        typescript: {
+          diagnosticOptions: {
+            semantic: true,
+            syntactic: true,
+          },
+        },
+        eslint: {
+          files: ['./src/**/*.{ts,tsx,js,jsx}']
+        }
+      })
+    ],
+    cache: {
+      type: 'memory',
+    },
+  };
+  return config;
+}
