e750803e86f396a2bc2c45727a48495ca611f818
Andreas Weber <weber.andreas@gmail.com>
2021-06-18 09:57:58 +0200

    refactor: removed eslint ignore

--- src/utils/notebookUtils.ts
+++ src/utils/notebookUtils.ts
@@ -1,4 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-explicit-any
-export function isNotebook(document: any) : boolean {
-  return document && !!document.notebook;
+import * as vscode from 'vscode';
+
+export function isNotebook(document: vscode.TextEditor): boolean {
+  const obj = document as { notebook?: unknown };
+  return obj && !!obj.notebook;
 }
